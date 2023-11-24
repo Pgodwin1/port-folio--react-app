f5a8c25ff82af4607b73b27f4675738354688c1a
Pgodwin1 <pgodwin287@gmail.com>
2023-11-24 14:14:31 +0100

    home page

---
+++ src/components/Home.tsx
@@ -0,0 +1,28 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <>
+    <div>
+        <div><h2>Godwin</h2></div>
+        <div> 
+            <a href="">About</a>
+            <a href="">Work</a>
+            <a href="">About</a>
+            <a href="">Contact</a>
+            <a href="">Download CV</a>
+        </div>     
+    </div>
+    <div>
+        <div><h2>Hi I'm Godwin</h2></div>
+        <div><p>A Full-stack Node.js/React software engineer with extensive experience and skill,<br/>
+             with a proven track record of providing effective solutions for tech-focused businesses. <br/>
+             I have developed my abilities over the past year to create and build durable applications <br/>
+              that improve productivity and user happiness. proficient in building scalable, <br/>
+              high-performance systems using cutting-edge technologies like TypeScript, Node.js, and React.</p></div>
+    </div>
+    </>
+  )
+}
+
+export default Home

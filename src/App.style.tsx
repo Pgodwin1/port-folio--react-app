2965bfece0213de0724c844f9a6bcc9ca96e0c7e
Pgodwin1 <pgodwin287@gmail.com>
2023-11-24 21:11:25 +0100

    header

---
+++ src/App.style.tsx
@@ -0,0 +1,52 @@
+import Styled from 'styled-components';
+
+export const Header = Styled.div`
+  background: #5A5A5A;
+  color: white;
+  height: 400px;
+  
+`
+export const Title = Styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding-top: 20px;
+  h2{
+    padding-left: 40px;
+  }
+`
+export const Nav = Styled.div`
+   
+a{
+  color: white;
+  text-decoration: none;
+  padding: 40px;
+}
+  
+`
+export const Profile = Styled.div`
+  margin-top: 40px;
+  margin-left: 40px;
+`
+export const Desc = Styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  .locate{
+    margin-top: 20px;
+    display: flex;
+  }
+  .icon{
+    margin-top: 20px;
+    gap: 30px;
+  }
+
+`
+export const Img = Styled.div`
+  margin-right: 60px;
+  height: 200px;
+  border: 10px;
+  img{
+    border-radius: 50%;
+  }
+`
--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,28 +1,50 @@
-import React from 'react'
+import React from 'react';
+import { IoLocationOutline } from "react-icons/io5";
+import { FaGithub } from "react-icons/fa";
+import { FaXTwitter } from "react-icons/fa6";
+import { FaLinkedin } from "react-icons/fa";
+import myprofile from '../assets/myprofile.jpeg'
+import { Header, Nav, Title, Desc, Profile, Img } from '../App.style'
 
 const Home = () => {
-  return (
-    <>
-    <div>
-        <div><h2>Godwin</h2></div>
-        <div> 
-            <a href="">About</a>
-            <a href="">Work</a>
-            <a href="">About</a>
-            <a href="">Contact</a>
-            <a href="">Download CV</a>
-        </div>     
-    </div>
-    <div>
-        <div><h2>Hi I'm Godwin</h2></div>
-        <div><p>A Full-stack Node.js/React software engineer with extensive experience and skill,<br/>
-             with a proven track record of providing effective solutions for tech-focused businesses. <br/>
-             I have developed my abilities over the past year to create and build durable applications <br/>
-              that improve productivity and user happiness. proficient in building scalable, <br/>
-              high-performance systems using cutting-edge technologies like TypeScript, Node.js, and React.</p></div>
-    </div>
-    </>
-  )
+    return (
+        <>
+            <Header>
+               <Title>
+                <h2>Godwin</h2>
+                <Nav>
+                    <a href="">About</a>
+                    <a href="">Work</a>
+                    <a href="">About</a>
+                    <a href="">Contact</a>
+                    <a href="">Download CV</a>
+                </Nav>
+                </Title>
+                <Profile>
+                <div><h2>Hi I'm Godwin</h2></div>
+            <Desc>
+                <div>
+                    <p>A Full-stack Node.js/React software engineer with extensive experience and skill,<br />
+                    with a proven track record of providing effective solutions for tech-focused businesses. <br />
+                    I have developed my abilities over the past year to create and build durable applications <br />
+                    that improve productivity and user happiness. proficient in building scalable, <br />
+                    high-performance systems using cutting-edge technologies like TypeScript, Node.js, and React.</p>
+                    <div className='locate'>
+                    <IoLocationOutline />
+                    <h4>Lagos, Nigeria</h4>
+                    </div>
+                    <div className='icon'>
+                    <FaGithub /><FaXTwitter /><FaLinkedin />
+                    </div>
+                 </div>
+                 <Img>
+                    <img src={myprofile} style={{width: "100%", height: "200px"}} alt="" />
+                 </Img>
+            </Desc>
+            </Profile>
+            </Header>
+        </>
+    )
 }
 
 export default Home

439a5bf115646d5ad034cbf5fb15c59b771f3da9
Pgodwin1 <pgodwin287@gmail.com>
2023-11-26 08:07:57 +0100

    about

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import Home from "./components/Home"
+import About from "./components/About"
 
 function App() {
 
   return (
     <>
       <Home />
+      <About />
     </>
   )
 }
---
+++ src/components/About.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const About = () => {
+    return (
+        <div>
+            <div>
+                <h2>About</h2>
+            </div>
+            <div>
+                <img src="" alt="" />
+            </div>
+            <div>
+                <p></p>
+            </div>
+        </div>
+    )
+}
+
+export default About
